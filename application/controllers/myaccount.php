612f702444712e34680c53153db6bbbf3848764e
bayusehat <aryabayu23@gmail.com>
2017-12-07 16:02:47 +0700

    Editor Plugin
    adding CKeditor

--- application/controllers/myaccount.php
+++ application/controllers/myaccount.php
@@ -7,6 +7,33 @@ class Myaccount extends CI_Controller {
 	{
 		$this->load->view('account_view');	
 	}
+	public function add_course(){
+		$this->load->view('add_course_view');
+	}
+	public function course_title(){
+
+		if($this->input->post('submit')){
+
+			$this->form_validation->set_rules('coursename', 'Course Name', 'trim|required');
+			$this->form_validation->set_rules('subject','Subject','trim|required');
+			// $this->form_validation->set_rules('thumnail', 'Thumbnail','trim|required');
+			$this->form_validation->set_rules('description','Description','trim|reuired');
+
+			if($this->form_validation->run()==TRUE){
+				if($this->course_model->add_course_title() == TRUE){
+					redirect('myaccount/add_course');
+				}else{
+					$this->session->set_flashdata('notif_failed','isi data dengan benar!');
+					redirect('myaccount');
+				}
+			}else{
+				$this->session->set_flashdata('notif_failed',validation_errors());
+				redirect('myaccount');
+			}
+		}
+
+		}
+	}
 
 }
 
